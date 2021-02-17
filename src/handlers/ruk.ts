aa65c4931cf026166f11faaf20775532de3eed50
Ravana <47528708+ravana69@users.noreply.github.com>
2021-02-17 10:56:53 +0530

    Update ruk.ts

--- src/handlers/ruk.ts
+++ src/handlers/ruk.ts
@@ -1,7 +1,7 @@
 import { Composer } from 'telegraf';
 import { pause } from '../tgcalls';
 
-export const pauseHandler = Composer.command(['ruk', 'resume'], async ctx => {
+export const pauseHandler = Composer.command(['ruk', 'chalu'], async ctx => {
     const { chat } = ctx.message;
 
     if (chat.type !== 'supergroup') {
@@ -9,7 +9,7 @@ export const pauseHandler = Composer.command(['ruk', 'resume'], async ctx => {
     }
 
     const paused = await pause(chat.id);
-    const message = paused === null ? "There's nothing playing here." : paused ? 'Paused.' : 'Resumed.';
+    const message = paused === null ? "Tera baap baja ke gaya kya." : paused ? 'ruk saale.' : 'chalu.';
 
     await ctx.reply(message);
 });
