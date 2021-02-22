f4db417860366da69ce2d7632ff8989a26e4b3ca
Ravana <47528708+ravana69@users.noreply.github.com>
2021-02-22 16:21:01 +0530

    Update dikha.ts

--- src/handlers/dikha.ts
+++ src/handlers/dikha.ts
@@ -12,7 +12,7 @@ export const queueHandler = Composer.command('dikha', ctx => {
     const message =
         queue && queue.length > 0
             ? queue.map((url, index) => `Haan bhaiya full dekh lo mera\n\n${index + 1}. ${url}`).join('\n')
-            : 'kal hi toh dikhaya tha full, firse dekh kyu dikhaun.';
+            : 'kal hi toh dikhaya tha full, firse kya dikhaun.';
 
     ctx.reply(message);
 });
