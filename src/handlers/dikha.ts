f1f209d904c80ae0cdee35b649f43aee9555f012
Ravana <47528708+ravana69@users.noreply.github.com>
2021-02-22 13:15:55 +0530

    Update dikha.ts

--- src/handlers/dikha.ts
+++ src/handlers/dikha.ts
@@ -11,8 +11,8 @@ export const queueHandler = Composer.command('dikha', ctx => {
     const queue = getQueue(chat.id);
     const message =
         queue && queue.length > 0
-            ? queue.map((url, index) => `${index + 1}. ${url}`).join('\n')
-            : 'o gamdu, tere ko kya dikhaun.';
+            ? queue.map((url, index) => `Haan bhaiya full dekh lo mera\n\n${index + 1}. ${url}`).join('\n')
+            : 'kal hi toh dikhaya tha full, firse dekh kyu dikhaun.';
 
     ctx.reply(message);
 });
